c42402db757c2865ed59100b311f3598e742a727
Aruna Turay <tura_arun@yahoo.com>
2023-05-25 10:35:45 -0400

    final commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,11 @@
 import React from 'react'
 
 
-function About({ image, about}) {
-  
+function About({ image, about }) {
   return (
     <aside>
-        {image ? (
-          <img src={image} alt="blog logo"/>
-        ) : (
-          "https://via.placeholder.com/215."
-        )}
-      
+      {image ? <img src={image} alt="blog logo" /> : "https://via.placeholder.com/215"}
+
       <p>{about}</p>
     </aside>
   );
