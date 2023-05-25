de0463349cdcedf01bf462da9c051f2a21190eeb
Aruna Turay <tura_arun@yahoo.com>
2023-05-25 14:55:02 -0400

    checking

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function About({ image = "https://via.placeholder.com/215", about }) {
   return (
     <aside>
-      {image ? <img src={image} alt="blog logo" /> : "https://via.placeholder.com/215" }
-   
+      {image ? <img src={image} alt="blog logo" /> : "https://via.placeholder.com/215"}
+
       <p>{about}</p>
     </aside>
   );
 }
 
-export default About
+export default About;
