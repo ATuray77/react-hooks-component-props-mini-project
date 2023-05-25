23bc06230c2aa445b851b0f8dee998652855c1e0
Aruna Turay <tura_arun@yahoo.com>
2023-05-25 11:05:44 -0400

    alters ternary

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,7 @@ import React from 'react'
 function About({ image, about }) {
   return (
     <aside>
-      {image ? <img src={image} alt="blog logo" /> : "https://via.placeholder.com/215"}
-
+      {image ? <img src={image} alt="blog logo" /> : ("https://via.placeholder.com/215") }
       <p>{about}</p>
     </aside>
   );
