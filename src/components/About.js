eb2075b94dc639e7968f1af7759872cbcbb634f8
Aruna Turay <tura_arun@yahoo.com>
2023-05-25 12:56:38 -0400

    adjusts the default value

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
 
-function About({ image, about }) {
+// function About({ image, about }) {
+//   return (
+//     <aside>
+//       {image ? <img src={image} alt="blog logo" /> : "https://via.placeholder.com/215" }
+   
+//       <p>{about}</p>
+//     </aside>
+//   );
+// }
+
+// export default About
+function About({ image = "https://via.placeholder.com/215", about }) {
   return (
     <aside>
-      {image ? <img src={image} alt="blog logo" /> : ("https://via.placeholder.com/215") }
+      {image ? <img src={image} alt="blog logo" /> : "https://via.placeholder.com/215" }
+   
       <p>{about}</p>
     </aside>
   );
